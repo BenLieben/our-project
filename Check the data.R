6a3f231bb49adf927c0de90cf6dd2d7d11ad041d
Ben Lieben <ben.lieben@student.uhasselt.be>
2022-04-01 08:07:01 +0200

    een begin

--- Check the data.R
+++ Check the data.R
@@ -14,27 +14,22 @@ attach(raw_data)
 
 #1)
 #First step in a replication are the descriptive tables.
-
-variable_list <- list(ChildID-hhID)
-#missing variables als NA laten??
-
+###############################################################################################
 #First create rules that establish sample eligibility;
 #Rule is 5 years old by 1990, so that they will be 19 by 2004;
 #Next restrict the sample to families with at least 2 age-eligible children;
 #Finally restrict to families where at least one (but not all) children were in Head Start;
-
+###############################################################################################
 #Replace missing age values with PPVT Age or age reported from the nearest survey year instead;
 
-#gen in stata is mutate in R
-#replace(x, list, values)     x = vactor haing some values, list = this can be an index vector, Values = the replacement values
-#if (test_expression) {
-#statement
-#}
-for(variable in variable_list){
-  a
-}
-  
+#sample <- raw_data%>%
+  #filter(PPVTAge90 == "120")
 
+#change all NA values to 0:
+raw_data[is.na(raw_data)] <- 0
+  
+#add all the ages(Age_Mo86 till Age_Mo104) in a new variable with mutate
+#age needs to be > 4 or >= 5 by 1990 so they are > 18 or >= 19 by 2004: use a filter to select the correct values
 
 
 
